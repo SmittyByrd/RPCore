42c7029a63ce91237ec0eb6e11a5d2df682cf6b7
SmittyByrd <byrdsmitty@gmail.com>
2026-06-11 22:38:42 -0500

    Register new configuration

--- src/main/java/com/github/smittybyrd/rpcore/RPCore.java
+++ src/main/java/com/github/smittybyrd/rpcore/RPCore.java
@@ -1,21 +1,42 @@
 package com.github.smittybyrd.rpcore;
 
 import com.github.smittybyrd.rpcore.commands.Walk;
+// import com.github.smittybyrd.rpcore.config.ConfigFile;
+import com.github.smittybyrd.rpcore.config.ConfigManager;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import static com.github.smittybyrd.rpcore.config.ConfigManager.*;
 
 public final class RPCore extends JavaPlugin {
 
+    private static RPCore plugin;
     @Override
     public void onEnable() {
         // Plugin startup logic
+        plugin = this;
+        new ConfigManager(this);
 
+        System.out.println(getModInput());
 
+        //saveResource("config.yml", false);
 
         getCommand("walk").setExecutor(new Walk());
+
+    }
+
+
+    public void onLoad() {
+
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static RPCore pluginGet() {
+        return plugin;
     }
 }
